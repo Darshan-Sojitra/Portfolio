605ea7089b70f890c91db0a7be761daae60926d1
Darshan-Sojitra <darshansojitra2003@gmail.com>
2025-06-27 11:00:50 +0530

    this is the clarity integration script

--- darshan-portfolio/src/app/page.tsx
+++ darshan-portfolio/src/app/page.tsx
@@ -8,7 +8,13 @@ import { Badge } from "@/components/ui/badge";
 import { DATA } from "@/data/resume";
 import Link from "next/link";
 import Markdown from "react-markdown";
-
+<script type="text/javascript">
+  (function(c,l,a,r,i,t,y){
+    c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "s5vzrhicv9");
+</script>
 const BLUR_FADE_DELAY = 0.04;
 
 export default function Page() {
